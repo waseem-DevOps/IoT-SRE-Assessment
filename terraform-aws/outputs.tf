fd818f5081522ebcfcfe612867b03c0902c725a5
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-24 18:45:06 +0300

    Update outputs.tf

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,1 +1,12 @@
+output "webapp_url" {
+  value = "http://www.${var.domain_name}"
+}
+
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
 

30e5c4bd1c87cd4bddcf9eb0820d9801b14188ef
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-24 18:44:27 +0300

    Update variables.tf

--- terraform-aws/variables.tf
+++ terraform-aws/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" { default = "us-east-1" }
+variable "vpc_cidr"    { default = "10.0.0.0/16" }
+variable "public_subnet_cidr" { default = "10.0.1.0/24" }
+variable "private_subnet_cidr" { default = "10.0.2.0/24" }
+
+variable "ami_id" {}
+variable "db_user" { default = "admin" }
+variable "db_pass" {}
+variable "db_name" { default = "webappdb" }
+variable "domain_name" {}
 

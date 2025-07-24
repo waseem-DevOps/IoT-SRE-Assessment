b07c6153843d37953353cbcc664997a5937528b1
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-24 18:47:26 +0300

    Update outputs.tf

--- terraform-aws/modules/vpc/outputs.tf
+++ terraform-aws/modules/vpc/outputs.tf
@@ -1,1 +1,5 @@
+output "vpc_id" { value = aws_vpc.this.id }
+output "public_subnet_id" { value = aws_subnet.public.id }
+output "private_subnet_id" { value = aws_subnet.private.id }
+output "sg_id" { value = aws_security_group.web_sg.id }
 

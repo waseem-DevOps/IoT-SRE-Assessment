296f36942b76246ea87a7a211fad56f0c7d84105
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-26 00:26:14 +0300

    Create subnets-varibales.tf

---
+++ terraform-aws/modules/subnets/subnets-varibales.tf
@@ -0,0 +1,15 @@
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+
+
+

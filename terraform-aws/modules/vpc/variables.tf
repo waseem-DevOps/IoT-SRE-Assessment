8865a3545aa87e050ace88d0aedcdd7b12c87ecf
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-26 00:04:47 +0300

    Update variables.tf

--- terraform-aws/modules/vpc/variables.tf
+++ terraform-aws/modules/vpc/variables.tf
@@ -1,4 +1,13 @@
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
+variable "customer" {
+    type = string
+    default = "iti"
+}
+variable "env_name" {
+    type = string
+    default = "dev"
+}
 
+variable "cidr_block_vpc" {
+    type = string
+    default = "10.0.0.0/16"
+}

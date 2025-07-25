0402e61fe387d4e4d263d9ecc7c04b8a105c6449
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-26 00:30:44 +0300

    Create security group.tf

---
+++ terraform-aws/modules/security group/security group.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "sg" {
+  name        = "new-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "new-sg"
+  }
+}
+

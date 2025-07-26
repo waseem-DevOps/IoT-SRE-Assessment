071794e2ee9c13edf6ae7b22725c7d85c84812ff
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-26 07:25:05 +0300

    Create ec2-variables.tf

---
+++ terraform-aws/modules/EC2/ec2-variables.tf
@@ -0,0 +1,22 @@
+variable "customer" {
+    type = string
+    default = "iti"
+  
+}
+
+variable "env_name" {
+    type = string
+    default = "dev"
+  
+}
+variable "subnet_id" {
+   
+}
+
+   
+variable "sg_id" {
+   
+}
+
+
+

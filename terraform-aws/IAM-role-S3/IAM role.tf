675bab768dab03632bebaed535bb1a351454b3a1
waseem-DevOps <Shilbywsym@gmail.com>
2025-07-26 15:12:25 +0300

    Create IAM role.tf

---
+++ terraform-aws/IAM-role-S3/IAM role.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "${var.role_name}-s3-access"
+  description = "Allow Lambda to access S3 bucket"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+}
